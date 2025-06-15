1cc2b80be02cad9f4113460c4b20c3030579730e
Pham Hoang Hao <phhao6798@gmail.com>
2025-06-15 17:27:48 +0700

    feat(auctions): create auction form - step 1

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import (
+    User,
+    Auction,
+    Bid,
+    Comment,
+    AuctionCategory,
+    AuctionCategoryMapping,
+    Watchlist,
+    AuctionImage,
+    ClosedAuction
+)
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Auction)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(AuctionCategory)
+admin.site.register(AuctionCategoryMapping)
+admin.site.register(Watchlist)
+admin.site.register(AuctionImage)
+admin.site.register(ClosedAuction)
--- auctions/views.py
+++ auctions/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Auction
 
 
 def index(request):
@@ -61,3 +61,37 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_auction(request):
+
+    if not request.user.is_authenticated:
+        return render(request, "auctions/login.html", {
+           "message": "You must be logged in to create an auction."
+    })
+
+    if request.method == "GET":
+        return render(request, "auctions/auction_form.html")
+                      
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
+        starting_bid = request.POST["starting_bid"]
+        end_date = request.POST["end_date"]
+        image_auction = request.POST.get("image_auction", None)
+        auction = Auction(
+            title=title,
+            description=description,
+            starting_bid=starting_bid,
+            end_date=end_date,
+            creator=request.user
+        )
+        if image_auction:
+            auction.image_auction = image_auction
+        else:
+            auction.image_auction = "https://placehold.co/600x400"
+        auction.save()
+        return render(request, "auctions/index.html", {
+            "message": "Auction created successfully!"
+        })
+
